7a01f9fa9452943cd6d7e279ca2a6147c5fb6ded
marklanderyou <marklanderyou@macbookpro.mshome>
2017-08-25 10:57:18 -0400

    updated porfolio sections

--- portfolio.php
+++ portfolio.php
@@ -21,17 +21,17 @@
     </article>
     <section>
       <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-3">
-        <li><img src="images/thumbs/LCC_thumb.png" alt="Landtech Computer Consulting"></li>
-        <li><img src="images/thumbs/LCC1_thumb.png" alt="Landtech Computer Consulting WordPress"></li>
-        <li><img src="images/thumbs/Energyworksnaturally_thumb.png" alt="Energy Works Naturally"></li>
-        <li><img src="images/thumbs/GDR_thumb.png" alt="Golf Destination Review"></li>
-        <li><img src="images/thumbs/THG_thumb.png" alt="The Hair Garage"></li>
-        <li><img src="images/thumbs/LDALC_thumb.png" alt="Learning Disabilities Assoc of Lambton County"></li>
-        <li><img src="images/thumbs/Clay_and_Gemma_thumb.png" alt="Clay and Gemma from SOA"></li>
-        <li><img src="images/thumbs/Jim_Morrison_1968-painting_thumb.png" alt="Young Jim Morrison"></li>
-        <li><img src="images/thumbs/yoda_thumb.jpg" alt="Master Yoda"></li>
-        <li><img src="images/thumbs/ketchupbottle_thumb.jpg" alt="Ketchup Bottle"></li>
-        <li><img src="images/thumbs/jukebox_thumb.jpg" alt="Jukebox"></li>
+        <li><img src="images/thumbs/LCC_thumb.png" class="web" alt="Landtech Computer Consulting"></li>
+        <li><img src="images/thumbs/LCC1_thumb.png" class="web" alt="Landtech Computer Consulting WordPress"></li>
+        <li><img src="images/thumbs/Energyworksnaturally_thumb.png" class="web" alt="Energy Works Naturally"></li>
+        <li><img src="images/thumbs/GDR_thumb.png" class="web" alt="Golf Destination Review"></li>
+        <li><img src="images/thumbs/THG_thumb.png" class="web" alt="The Hair Garage"></li>
+        <li><img src="images/thumbs/LDALC_thumb.png" class="web" alt="Learning Disabilities Assoc of Lambton County"></li>
+        <li><img src="images/thumbs/Clay_and_Gemma_thumb.png" class="illustration" alt="Clay and Gemma from SOA"></li>
+        <li><img src="images/thumbs/Jim_Morrison_1968-painting_thumb.png" class="illustration" alt="Young Jim Morrison"></li>
+        <li><img src="images/thumbs/yoda_thumb.jpg" class="illustration" alt="Master Yoda"></li>
+        <li><img src="images/thumbs/ketchupbottle_thumb.jpg" class="photoshop" alt="Ketchup Bottle"></li>
+        <li><img src="images/thumbs/jukebox_thumb.jpg" class="photoshop" alt="Jukebox"></li>
       </ul>
     </section>
    
