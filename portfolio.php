dc9c165b7aaaa542a8aac06e3ec269a8b2ecb4aa
marklanderyou <marklanderyou@macbookpro.mshome>
2017-08-25 10:06:04 -0400

    Version 2

--- portfolio.php
+++ portfolio.php
@@ -2,8 +2,6 @@
   $pageTitle = "Portfolio";
 	require_once('admin/includes/init.php');
 	
-	//$tbl = "tbl_portfolio";
-	//$getPortfolio = getAll($tbl);
   include "includes/header.php";
   include "includes/nav.php";
 ?>
@@ -36,53 +34,7 @@
         <li><img src="images/thumbs/jukebox_thumb.jpg" alt="Jukebox"></li>
       </ul>
     </section>
-    <section>
-      <div class="row small-up-1 medium-up-2 large-up-4">
-      
-  <!-- <div class="column"> -->
-  
-    <!-- <!-- <img src="images/thumbs/LCC_thumb.png" class="thumbnail" alt="Landtech Computer Consulting"> -->
-<!--   </div> -->
-  <!-- <div class="column"> -->
-    <!-- <img src="images//thumbs/LCC1_thumb.png" class="thumbnail" alt="Landtech Computer Consulting Wordpress">
-  </div> -->
-  <!-- <div class="column">
-    <img src="images/thumbs/Energyworksnaturally_thumb.png" class="thumbnail" alt="Energy Works Naturally Wordpress">
-  </div> -->
-  <!-- <div class="column">
-    <img src="images/thumbs/GDR_thumb.png" class="thumbnail" alt="Golf Destination Review">
-  </div> -->
-  <!-- <div class="column">
-    <img src="images/thumbs/THG_thumb.png" class="thumbnail" alt="The Hair Garage Wordpress">
-  </div> -->
-  <!-- <div class="column">
-    <img src="images/thumbs/LDALC_thumb.png" class="thumbnail" alt="Learning Disabilities Assoc of Lambton Country">
-  </div> -->
-  <!-- <div class="column">
-    <img src="images/thumbs/Clay_and_Gemma_thumb.png" class="thumbnail" alt="Clay and Gemma">
-  </div> -->
-  <!-- <div class="column">
-    <img src="images/thumbs/Jim_Morrison_1968-painting_thumb.png" class="thumbnail" alt="Young Jim Morrison">
-  </div> -->
-  <!-- <div class="column">
-    <img src="images/thumbs/yoda_thumb.jpg" class="thumbnail" alt="Master Yoda">
-  </div> -->
-  <!-- <div class="column">
-    <img src="images/thumbs/ketchupbottle_thumb.jpg" class="thumbnail" alt="Ketchup Bottle">
-  </div> -->
-  <!-- <div class="column">
-    <img src="images/thumbs/jukebox_thumb.jpg" class="thumbnail" alt="Jukebox">
-  </div>
-</div> -->
-<!-- <div class="large reveal" id="exampleModal1" data-reveal>
-  <h2>Landtech Computer Consulting</h2>
-  <img class="float-left" src="images/LCC.png" alt="Landtech Computer Consulting">
-  <p class="lead">This Site was done in HTML5 and CSS3</p>
-  <button class="close-button" data-close aria-label="Close reveal" type="button">
-    <span aria-hidden="true">&times;</span>
-  </button> --> 
-</div>
-    </section>
+   
     <?php 
       include "includes/socialmediasection.php";
       include "includes/footer.php";
