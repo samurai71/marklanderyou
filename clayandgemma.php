a78fc9426f72958e4be1d0d4a636f34104f0073c
marklanderyou <marklanderyou@macbookpro.mshome>
2017-08-25 13:05:04 -0400

    back next and home buttons

--- clayandgemma.php
+++ clayandgemma.php
@@ -17,6 +17,18 @@
             </div>
         </div>
     </article>
+    <section class="navBtns">
+        <div class="row">
+            <div class="small-12 medium-offset-4 medium-4 large-offset-4 large-4 columns">
+                <a href="ldalc.php"><img src="images/back_btn.svg" alt="Back Button" title="Back Button"></a>
+                <a href="portfolio.php"><img src="images/home_btn.svg" alt="Home Button" title="Home Button"></a>
+                <a href="jimmorrison.php"><img src="images/next_btn.svg" alt="Next Button" title="Next Button"></a>
+                
+
+            </div>
+        </div>
+        
+    </section>
     <?php 
       include "includes/socialmediasection.php";
       include "includes/footer.php";
