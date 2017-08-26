7854b8ac41a98022dc84e48d79f0b085342b8d46
marklanderyou <marklanderyou@macbookpro.mshome>
2017-08-26 13:55:18 -0400

    combined all css styles sheets

--- includes/header.php
+++ includes/header.php
@@ -7,12 +7,13 @@
     <meta name="description" content="Mark Landeryou is an artisan from London Ontario, Canada. Who prides himself in the quality of his work">
     <meta name="keywords" content="Mark Landeryou Artisan, London Ontario Canada">
     <meta name="author" content="Mark Landeryou">
-    <link rel="stylesheet" href="css/normalize.css" />
+    <!-- <link rel="stylesheet" href="css/normalize.css" />
     <link rel="stylesheet" href="css/foundation.min.css" />
     <link rel="stylesheet" href="css/app.css" />
     <link rel="stylesheet" href="css/slicknav.min.css" />
     <link rel="stylesheet" href="css/imagehover.min.css" />
-    <link rel="stylesheet" href="css/icons.min.css" />
+    <link rel="stylesheet" href="css/icons.min.css" /> -->
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="icon" href="favicon.ico" type="image/x-icon"/>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js"></script>
     <script src="js/vendor/modernizr.js"></script>
